4a915e0447a83492947ce3f13e6144ebd318e19a
Patricia Santana <Patricia.santana@easypayfinance.com>
2022-03-30 12:27:48 -0600

    gaby 1st

---
+++ app.js
@@ -0,0 +1,60 @@
+const boton = document.querySelector('#button');
+const menuS = document.querySelector('#menuSmall');
+const closes = document.querySelector('#close');
+const newM = document.querySelector('#new');
+const newMenu = document.querySelector('#newMenu');
+
+boton.addEventListener('click', () => {
+    console.log('hiciste click')
+    menuS.classList.toggle('hidden');
+});
+
+newM.addEventListener('click', () => {
+    newMenu.classList.toggle('hidden')
+});
+
+function nextSlide(){
+    let activeSlide = document.querySelector('.slide.translate-x-0');
+    activeSlide.classList.remove('translate-x-0');
+    activeSlide.classList.add('-translate-x-full');
+    
+    let nextSlide = activeSlide.nextElementSibling;
+    nextSlide.classList.remove('translate-x-full');
+    nextSlide.classList.add('translate-x-0');
+}
+
+function previousSlide(){
+    let activeSlide = document.querySelector('.slide.translate-x-0');
+    activeSlide.classList.remove('translate-x-0');
+    activeSlide.classList.add('translate-x-full');
+    
+    let previousSlide = activeSlide.previousElementSibling;
+ 
+};
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: theme => ({
+        'principal': "url('img/principal.png')",
+        'icon': "url('img/karenkane.png')",
+        'first': "url('img/img1.png')",
+        'second': "url('img/img2.webp')",
+        'third': "url('img/img3.webp')",
+        'fourth': "url('img/img4.webp')",
+        'phone': "url('img/phone.svg')",
+        'email': "url('img/email.svg')",
+        'corazon': "url('img/corazon.svg')",
+        'stars': "url('img/stars.svg')",
+        'slide1': "url('img/slede1.webp')",
+        'slide2': "url('img/slide2.webp')",
+        'collar': "url('img/collar.webp')",
+        'mujer':"url('img/mujer.webp')",
+        'todos': "url('img/todos.png')",
+        'dos': "url('img/dos.png')",
+        'unapersona1': "url('img/unapersona1.png')",
+        'unapersona2': "url('img/unapersona2.png')",
+        'unapersona3': "url('img/unapersona3.png')",
+
+      }),
+    },
+    colors: {
+      'beige': '#FCFBF8',
+      'blue': '#339CFF',
+      'green': '#21A76B',
+      'pink': '#ED71EB',
+      'gray': '#616365',
+      'ligthBeige': '#FCFBF7',
+      'grayText': '#3A3A3A',
+    },
+  },
+  plugins: [],
+}
