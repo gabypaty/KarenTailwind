e427b306550d0203722d5fd5bc455f2345d4eaa5
Patricia Santana <Patricia.santana@easypayfinance.com>
2022-04-01 14:36:44 -0600

    sticky menu

--- app.js
+++ app.js
@@ -3,33 +3,41 @@ const menuS = document.querySelector('#menuSmall');
 const closes = document.querySelector('#close');
 const newM = document.querySelector('#new');
 const newMenu = document.querySelector('#newMenu');
+const menuStick = document.querySelector('#sticky2');
+
+
+document.getElementById('container').onscroll = function () {
+  console.log("scrolling");
+  menuStick.classList.toggle('hidden')
+};
 
 boton.addEventListener('click', () => {
-    console.log('hiciste click')
-    menuS.classList.toggle('hidden');
+  console.log('hiciste click')
+  menuS.classList.toggle('hidden');
 });
 
 newM.addEventListener('click', () => {
-    newMenu.classList.toggle('hidden')
+  newMenu.classList.toggle('hidden')
 });
 
-function nextSlide(){
-    let activeSlide = document.querySelector('.slide.translate-x-0');
-    activeSlide.classList.remove('translate-x-0');
-    activeSlide.classList.add('-translate-x-full');
-    
-    let nextSlide = activeSlide.nextElementSibling;
-    nextSlide.classList.remove('translate-x-full');
-    nextSlide.classList.add('translate-x-0');
+// Slider 1
+function nextSlide() {
+  let activeSlide = document.querySelector('.slide.translate-x-0');
+  activeSlide.classList.remove('translate-x-0');
+  activeSlide.classList.add('-translate-x-full');
+
+  let nextSlide = activeSlide.nextElementSibling;
+  nextSlide.classList.remove('translate-x-full');
+  nextSlide.classList.add('translate-x-0');
 }
 
-function previousSlide(){
-    let activeSlide = document.querySelector('.slide.translate-x-0');
-    activeSlide.classList.remove('translate-x-0');
-    activeSlide.classList.add('translate-x-full');
-    
-    let previousSlide = activeSlide.previousElementSibling;
- 
+function previousSlide() {
+  let activeSlide = document.querySelector('.slide.translate-x-0');
+  activeSlide.classList.remove('translate-x-0');
+  activeSlide.classList.add('translate-x-full');
+
+  let previousSlide = activeSlide.previousElementSibling;
+
 };
 
 let slideIndex = 1;
@@ -47,38 +55,39 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
 
+//Slider 1 termina
 
-let slideImage = 1;
-showImages(slideImage);
+//Slider 2 
 
-function next(n) {
-  showImages(slideImage += n);
-}
+let image = 0;
+showS();
 
-function current(n) {
-  showImages(slideImage = n);
-}
-
-function showImages(n) {
-  let im;
-  let images = document.getElementsByClassName("imageSlides");   
-  if (n < 1) {slideImage = images.length}
-  for (im = 0; im < images.length; im++) {
-    images[im].style.display = "none";  
+function showS() {
+  let i;
+  let images = document.getElementsByClassName("mySlider");
+  let dots = document.getElementsByClassName("dot");
+  console.log('img', images.length)
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
   }
-  
-  images[slideImage-1].style.display = "block";  
- 
+  image++;
+  if (image > images.length) { image = 1 }
+
+  images[image - 1].style.display = "block";
+  setTimeout(showS, 2000); // Change image every 2 seconds
 }
+//FIN SLIDER 2
+
+
